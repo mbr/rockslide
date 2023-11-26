a87e87d01684caed62a3d2f76b49c24e2c6493cd
Marc Brinkmann <git@marcbrinkmann.de>
2023-11-26 18:23:57 +0100

    More exploratory code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    body::Body,
+    http::{header::LOCATION, Request, Response, StatusCode},
+    response::{Html, IntoResponse},
+    routing::{get, post},
+    Router,
+};
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+// https://github.com/distribution/distribution/blob/5cb406d511b7b9163bff9b6439072e4892e5ae3b/docs/spec/api.md
 
 #[tokio::main]
 async fn main() {
@@ -17,7 +25,8 @@ async fn main() {
 
     // build our application with a route
     let app = Router::new()
-        .route("/", get(handler))
+        .route("/v2/", get(index_v2))
+        .route("/v2/test/blobs/uploads/", post(upload_blob_test))
         .layer(TraceLayer::new_for_http());
 
     // run it with hyper on localhost:3000
@@ -27,6 +36,16 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn index_v2() -> Html<&'static str> {
+    // Login will just store credentials locally, not do anything with them. This is all we need.
+    Html("all systems operational")
+}
+
+async fn upload_blob_test(request: Request<Body>) -> Response<Body> {
+    let mut resp = StatusCode::ACCEPTED.into_response();
+    let location = format!("/v2/test/blobs/uploads/asdf123"); // TODO: should be uuid
+    resp.headers_mut()
+        .append(LOCATION, location.parse().unwrap());
+
+    resp.map(|_| Default::default())
 }
