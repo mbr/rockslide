bed47373b7eac58d6be1a556191755580e61c759
Marc Brinkmann <git@marcbrinkmann.de>
2023-12-30 16:56:36 +0100

    Write new routing function that also routes domains

--- src/reverse_proxy.rs
+++ src/reverse_proxy.rs
@@ -3,20 +3,25 @@ use std::{
     fmt::{self, Display},
     mem,
     net::SocketAddr,
+    str::FromStr,
     sync::Arc,
 };
 
 use axum::{
     body::Body,
     extract::{Request, State},
-    http::StatusCode,
+    http::{
+        uri::{Authority, Scheme},
+        StatusCode, Uri,
+    },
     response::{IntoResponse, Response},
-    routing::any,
     Router,
 };
 use itertools::Itertools;
 use tokio::sync::RwLock;
 use tracing::{trace, warn};
+
+use crate::registry::storage::ImageLocation;
 
 pub(crate) struct ReverseProxy {
     client: reqwest::Client,
@@ -39,6 +44,11 @@ impl RoutingTable {
     #[inline(always)]
     fn get_path_route(&self, image_location: &ImageLocation) -> Option<&PublishedContainer> {
         self.path_maps.get(image_location)
+    }
+
+    #[inline(always)]
+    fn get_domain_route(&self, domain: &Domain) -> Option<&PublishedContainer> {
+        self.domain_maps.get(domain)
     }
 }
 
@@ -144,11 +154,7 @@ impl ReverseProxy {
     }
 
     pub(crate) fn make_router(self: Arc<Self>) -> Router {
-        Router::new()
-            .route("/:repository/:image", any(reverse_proxy))
-            .route("/:repository/:image/", any(reverse_proxy))
-            .route("/:repository/:image/*remainder", any(reverse_proxy))
-            .with_state(self)
+        Router::new().fallback(route_request).with_state(self)
     }
 
     pub(crate) async fn update_containers(
@@ -162,63 +168,85 @@ impl ReverseProxy {
     }
 }
 
-async fn reverse_proxy(
+async fn route_request(
     State(rp): State<Arc<ReverseProxy>>,
     request: Request,
-) -> Result<Response<Body>, AppError> {
-    // Determine rewritten URL.
+) -> Result<Response, AppError> {
     let req_uri = request.uri();
 
-    let mut segments = req_uri
-        .path()
-        .split('/')
-        .filter(|segment| !segment.is_empty());
+    let opt_domain = req_uri.host().and_then(Domain::new);
+    let routing_table = rp.routing_table.read().await;
 
-    let image_location = ImageLocation::new(
-        segments
-            .next()
-            .ok_or(AppError::AssertionFailed("repository segment disappeared"))?
-            .to_owned(),
-        segments
-            .next()
-            .ok_or(AppError::AssertionFailed("image segment disappeared"))?
-            .to_owned(),
-    );
+    let dest_uri = if let Some(pc) =
+        opt_domain.and_then(|domain| routing_table.get_domain_route(&domain))
+    {
+        // We only need to swap the protocol and domain and we're good to go.
+        let mut parts = req_uri.clone().into_parts();
+        parts.scheme = Some(Scheme::HTTP);
+        parts.authority = Some(Authority::from_str(&pc.host_addr.to_string()).map_err(|_| {
+            anyhow::anyhow!("failed to convert container address into `Authority`")
+        })?);
+        Some(
+            Uri::from_parts(parts)
+                .map_err(|_| anyhow::anyhow!("did not expect invalid uri parts"))?
+                .to_string(),
+        )
+    } else {
+        // Reconstruct image location from path segments, keeping remainder intact.
+        let mut segments = req_uri
+            .path()
+            .split('/')
+            .filter(|segment| !segment.is_empty());
+
+        let image_location = ImageLocation::new(
+            segments
+                .next()
+                .ok_or(AppError::AssertionFailed("repository segment disappeared"))?
+                .to_owned(),
+            segments
+                .next()
+                .ok_or(AppError::AssertionFailed("image segment disappeared"))?
+                .to_owned(),
+        );
+
+        if let Some(pc) = routing_table.get_path_route(&image_location) {
+            let container_addr = pc.host_addr;
+
+            // Now create the path, format is: '' / repository / image / ...
+            // Need to skip the first three.
+            let cleaned_path = segments.join("/");
+
+            let mut dest_path_and_query = cleaned_path;
+
+            if req_uri.path().ends_with('/') {
+                dest_path_and_query.push('/');
+            }
+
+            if let Some(query) = req_uri.query() {
+                dest_path_and_query.push('?');
+                dest_path_and_query += query;
+            }
+
+            // Reassemble
+            Some(format!("http://{container_addr}/{dest_path_and_query}"))
+        } else {
+            None
+        }
+    };
+
+    // Release lock.
+    drop(routing_table);
 
     // TODO: Return better error (404?).
-    let dest_addr = rp
-        .routing_table
-        .read()
-        .await
-        .get_path_route(&image_location)
-        .ok_or(AppError::NoSuchContainer)?
-        .host_addr;
-    let base_url = format!("http://{dest_addr}");
-
-    // Format is: '' / repository / image / ...
-    // Need to skip the first three.
-    let cleaned_path = segments.join("/");
-
-    let mut dest_path_and_query = cleaned_path;
-
-    if req_uri.path().ends_with('/') {
-        dest_path_and_query.push('/');
-    }
-
-    if let Some(query) = req_uri.query() {
-        dest_path_and_query.push('?');
-        dest_path_and_query += query;
-    }
-
-    let dest_uri = format!("{base_url}/{dest_path_and_query}");
-    trace!(%dest_uri, "reverse proxying");
+    let dest = dest_uri.ok_or(AppError::NoSuchContainer)?;
+    trace!(%dest, "reverse proxying");
 
     // Note: `reqwest` and `axum` currently use different versions of `http`
     let method =
         request.method().to_string().parse().map_err(|_| {
             AppError::AssertionFailed("method http version mismatch workaround failed")
         })?;
-    let response = rp.client.request(method, &dest_uri).send().await;
+    let response = rp.client.request(method, &dest).send().await;
 
     match response {
         Ok(response) => {
@@ -238,7 +266,7 @@ async fn reverse_proxy(
                 .map_err(|_| AppError::AssertionFailed("should not fail to construct response"))?)
         }
         Err(err) => {
-            warn!(%err, %dest_uri, "failed request");
+            warn!(%err, %dest, "failed request");
             Ok(Response::builder()
                 .status(500)
                 .body(Body::empty())
@@ -264,5 +292,3 @@ mod hop_by_hop {
     ];
 }
 use hop_by_hop::HOP_BY_HOP;
-
-use crate::registry::storage::ImageLocation;
