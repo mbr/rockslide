1b7c72f870485d612bfb01d7b7c21f6756282b5b
Marc Brinkmann <git@marcbrinkmann.de>
2024-01-18 18:12:50 +0100

    Make `quick-test.sh` curl the `testing/hello` page

--- quick-test.sh
+++ quick-test.sh
@@ -12,3 +12,7 @@ podman login --tls-verify=false --username devuser --password devpw http://${REG
 podman pull crccheck/hello-world
 podman tag crccheck/hello-world ${REGISTRY_ADDR}/testing/hello:prod
 podman push --tls-verify=false ${REGISTRY_ADDR}/testing/hello:prod
+
+sleep 2
+
+curl -v http://${REGISTRY_ADDR}/testing/hello
